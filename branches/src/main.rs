556a61bd7250f5345119ecb84abc4281381c9160
Trojan-254 <samwuels254@gmail.com>
2024-01-07 22:30:40 +0300

    control_flow

---
+++ branches/src/main.rs
@@ -0,0 +1,13 @@
+// Control flow
+
+// If expressions
+
+fn main() {
+    let number = 7;
+    if number != 7{
+        println!("condition was true");
+    } else {
+        println!("condition was false")
+    }
+    // println!("Hello, world!");
+}
