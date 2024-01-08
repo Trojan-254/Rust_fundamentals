31394ceebc25a7795f4e936e49260afc812a7145
Trojan-254 <samwuels254@gmail.com>
2024-01-08 06:10:41 +0300

    if control flow

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,13 +1,36 @@
 // Control flow
-
+use std::io;
 // If expressions
 
 fn main() {
-    let number = 7;
-    if number != 7{
-        println!("condition was true");
-    } else {
-        println!("condition was false")
+    loop {
+
+        println!("Please enter your input");
+        println!(" ");
+        let mut number = String::new();
+
+    io::stdin()
+        .read_line(  &mut number)
+        .expect("failed to read line");
+
+    let num: Result<i32, _> = number.trim().parse();
+
+    match num {
+        Ok(parsed_num) => {
+            if parsed_num % 4 == 0{
+                println!("number is divisible by 4");
+            } else if parsed_num % 3 == 0 {
+                println!("Number is divisible by 3");
+            } else if parsed_num % 2 == 0 {
+                println!("Number is divisible by 2");
+            } else {
+                println!("Number is not divisible by 4, 3, or 2");
+            }
+        }
+        Err(_) => {
+            println!("Invalid input, please input a valid integer")
+        }
     }
-    // println!("Hello, world!");
+
+    }
 }
